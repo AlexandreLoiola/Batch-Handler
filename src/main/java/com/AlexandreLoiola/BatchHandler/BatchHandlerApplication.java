0f91a092f9b93648c9915b97125a5ee92451ddf8
Alexandre Loiola <alexandre.loiola.741@gmail.com>
2024-12-17 03:20:03 -0300

    Configuracao Inicial

---
+++ src/main/java/com/AlexandreLoiola/BatchHandler/BatchHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.AlexandreLoiola.BatchHandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchHandlerApplication.class, args);
+	}
+
+}
